428a36dfa25174416af5707e24a005a7be1a2d16
Romain Pierre Marie Denis <ps04egl@eduvaud.ch>
2025-04-04 14:00:06 +0200

    chore(Rename Project)
    - Renamed project from AddPages to Read4All, since AddPages was an exercise i copied to get the base

---
+++ App/Read4All/Read4All/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Read4All
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
