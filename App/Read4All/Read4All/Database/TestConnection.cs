4166e7ff0c2f3c7d1f9ad8f3d4c7b12607068b67
Romain Pierre Marie Denis <ps04egl@eduvaud.ch>
2025-04-11 14:43:47 +0200

    feat(Database Connection)
    - Connection to the database now works

---
+++ App/Read4All/Read4All/Database/TestConnection.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Maui.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+using MySql.Data.MySqlClient;
+using System.Diagnostics;
+
+namespace Read4All.Database
+{
+    class TestConnection
+    {
+        public async Task TestConnectionAsync()
+        {
+            try
+            {
+                using (var connection = new MySqlConnection("Server=10.0.2.2;Port=6033;Database=db_livre;User=root;Password=root;"))
+                {
+                    await connection.OpenAsync();
+                    Debug.WriteLine("Works " + connection.ConnectionString);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Doesnt work lol" + ex);
+            }
+        }
+    }
+}
--- App/Read4All/Read4All/MainPage.xaml.cs
+++ App/Read4All/Read4All/MainPage.xaml.cs
@@ -1,12 +1,21 @@
+using Read4All.Database;
 using System.Collections.ObjectModel;
 
 namespace Read4All
 {
     public partial class MainPage : ContentPage
-    { 
+    {
+        private readonly TestConnection testconnection;
         public MainPage()
         {
             InitializeComponent();
+            testconnection = new TestConnection();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            await testconnection.TestConnectionAsync();
         }
     }
 }
